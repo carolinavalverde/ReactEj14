676dcb9c2e319d0d323242ff2ebc4ff4f9b64d24
Caro Valverde <carolina.valverde255@gmail.com>
2024-02-27 21:36:03 -0300

    agrego img de recetas

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -25,11 +25,6 @@ const Inicio = () => {
 
   return (
     <section className="mainSection">
-      {/* <img
-        className="banner"
-        src="https://images.pexels.com/photos/13591748/pexels-photo-13591748.jpeg"
-        alt="fondo cafe"
-      /> */}
       <Container className="mt-5">
         <h1 className="display-4 text-center">Mis Recetas</h1>
         <hr />
