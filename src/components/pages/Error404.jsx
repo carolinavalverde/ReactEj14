16bf2753d07ac3b4d2ad2a3bda2b66d8848054ae
Caro Valverde <carolina.valverde255@gmail.com>
2024-03-02 16:03:53 -0300

    agrego css a error404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,17 +1,25 @@
-import { Button } from 'react-bootstrap';
-import error from '../../assets/error404.jpg'
-import { Link } from 'react-router-dom';
+import { Button } from "react-bootstrap";
+import error from "../../assets/error404.jpg";
+import { Link } from "react-router-dom";
 
 const Error404 = () => {
-    return (
-        <section className="mainSection text-center">
-        <img src={error} alt="error 404" />
-        <div className='mb-4'>
-        <Button variant='success' as={Link} to="/">Volver al inicio</Button>
-
+  return (
+    <section className="mainSection text-center">
+      <img src={error} alt="error 404" />
+      <div className="my-4">
+        <span className="display-6 m-4">Parece que ésta vez no podrá ser</span>
+        <br />
+        <span className="display-6 m-4">
+          Intentá en otro momento o volvé al inicio
+        </span>
+        <div className=" d-flex justify-content-center">
+          <Button variant="success" className="p-3 m-4" as={Link} to="/">
+            Volver al inicio
+          </Button>
         </div>
+      </div>
     </section>
-    );
+  );
 };
 
 export default Error404;
